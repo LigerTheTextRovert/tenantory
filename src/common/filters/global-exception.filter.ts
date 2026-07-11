f0aa02200e2b13190024c706665690d1afbfdcf9
LiGER <samarghandi112w34@gmail.com>
2026-07-11 23:18:27 +0330

    chore: added option object to the validation pipe

---
+++ src/common/filters/global-exception.filter.ts
@@ -0,0 +1,54 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const request = context.getRequest<Request>();
+    const response = context.getResponse<Response>();
+
+    const isHttpException = exception instanceof HttpException;
+
+    const status = isHttpException
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const exceptionResponse = isHttpException ? exception.getResponse() : null;
+
+    let message = 'internal server error';
+
+    if (typeof exceptionResponse === 'string') {
+      message = exceptionResponse;
+    } else if (
+      exceptionResponse &&
+      typeof exceptionResponse === 'object' &&
+      'message' in exceptionResponse
+    ) {
+      // message = (exceptionResponse as { message: string | string[] }).message;
+      message = 'temp message';
+    }
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+
+      ...(process.env.NODE_ENV !== 'production' &&
+        exception instanceof Error && {
+          stack: exception.stack,
+        }),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   app.enableVersioning({
     type: VersioningType.URI,
