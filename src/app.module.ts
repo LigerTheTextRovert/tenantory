2c308eac610a3e6b07c98e62c99252636a707dc1
LiGER <samarghandi112w34@gmail.com>
2026-07-11 16:17:56 +0330

    feat: added database config and .env variables config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TenantModule } from './tenant/tenant.module';
+import { CatalogModule } from './catalog/catalog.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { WarehouseModule } from './warehouse/warehouse.module';
+import { SupplierModule } from './supplier/supplier.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { dataSourceOption } from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOption),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TenantModule,
+    CatalogModule,
+    InventoryModule,
+    WarehouseModule,
+    SupplierModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
