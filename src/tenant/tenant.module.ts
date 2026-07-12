8fd23effb236363c8cc0b78bf1cdf136a7e70422
LiGER <samarghandi112w34@gmail.com>
2026-07-12 19:08:06 +0330

    chore: attached the tenant service to tenant module

--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tenant } from './entities/tenant.entity';
+import { TenantService } from './tenant.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tenant])],
-  exports: [TypeOrmModule],
+  providers: [TenantService],
+  exports: [TypeOrmModule, TenantService],
 })
 export class TenantModule {}
