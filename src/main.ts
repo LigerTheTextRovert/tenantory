00c182acd8bdb28cf7611dd7b535e509f6735b98
LiGER <samarghandi112w34@gmail.com>
2026-07-12 23:51:04 +0330

    chore: wire the main.ts with interceptor and guard for tenant

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { TenantGuard } from './tenant/tenant.guard';
+import { TenantInterceptor } from './tenant/tenant.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +19,9 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.useGlobalGuards(app.get(TenantGuard));
+  app.useGlobalInterceptors(app.get(TenantInterceptor));
 
   await app.listen(process.env.PORT ?? 3000);
 }
