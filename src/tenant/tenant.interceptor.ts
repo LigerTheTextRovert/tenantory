df371470b86490f2a97ac3602ebb43fc075fe0fa
LiGER <samarghandi112w34@gmail.com>
2026-07-13 12:26:20 +0330

    fix: fix the undefined tenantId creation by returning in if block, and renamed ctx to context for better readability

--- src/tenant/tenant.interceptor.ts
+++ src/tenant/tenant.interceptor.ts
@@ -10,12 +10,12 @@ import { tenantAsyncStorage } from './tenant-context';
 
 @Injectable()
 export class TenantInterceptor implements NestInterceptor {
-  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
-    const req = ctx.switchToHttp().getRequest<TenantRequest>();
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest<TenantRequest>();
     const tenantId = req.tenantId;
 
     if (!tenantId) {
-      next.handle();
+      return next.handle();
     }
 
     return tenantAsyncStorage.run({ tenantId }, () => next.handle());
