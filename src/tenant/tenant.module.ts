ed13797149bf1d4d59bca05b5912943040fb453d
LiGER <samarghandi112w34@gmail.com>
2026-07-17 23:24:04 +0330

    fix: added the TenantGaurd and TenantInterceptor to providers array

--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tenant } from './entities/tenant.entity';
 import { TenantService } from './tenant.service';
+import { TenantGuard } from './tenant.guard';
+import { TenantInterceptor } from './tenant.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tenant])],
-  providers: [TenantService],
+  providers: [TenantService, TenantGuard, TenantInterceptor],
   exports: [TypeOrmModule, TenantService],
 })
 export class TenantModule {}
