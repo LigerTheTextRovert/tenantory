81fb31675dab6f37731064a0981b8fb7c75b24c0
LiGER <samarghandi112w34@gmail.com>
2026-07-17 23:23:47 +0330

    fix: removed the db config and used forRootAsync instead

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,34 @@ import { WarehouseModule } from './warehouse/warehouse.module';
 import { SupplierModule } from './supplier/supplier.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { dataSourceOption } from './config/db.config';
+// import { dataSourceOption } from './config/db.config';
 import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
 import { TenantMiddleware } from './tenant/tenant.middleware';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({ ...dataSourceOption, autoLoadEntities: true }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT_EXTERNAL),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+
+        autoLoadEntities: true,
+
+        synchronize: process.env.DB_SYNCHRONIZE === 'true',
+        logging: process.env.NODE_ENV === 'development',
+
+        namingStrategy: new SnakeNamingStrategy(),
+        cache: true,
+      }),
     }),
     TenantModule,
     CategoryModule,
