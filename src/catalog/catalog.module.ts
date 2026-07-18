d51685711696a9ec74aac072f59725b8a0fac648
LiGER <samarghandi112w34@gmail.com>
2026-07-18 21:24:09 +0330

    chore: import the category.module to catalog.module in order to inject category.service

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductVariant } from './entities/product-variant.entity';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductVariant])],
+  imports: [
+    TypeOrmModule.forFeature([Product, ProductVariant]),
+    CategoryModule,
+  ],
   exports: [TypeOrmModule],
 })
 export class CatalogModule {}
