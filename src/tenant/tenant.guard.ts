391b4659800fcd9e636ec384012babf7c3562e8d
LiGER <samarghandi112w34@gmail.com>
2026-07-20 18:52:01 +0330

    feat:implement @Public decorator to tell tenant.guard to skip authentication

--- src/tenant/tenant.guard.ts
+++ src/tenant/tenant.guard.ts
@@ -4,12 +4,25 @@ import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { TenantRequest } from './tenant.middleware';
 import { TenantStatus } from './entities/tenant.entity';
+import { IS_PUBLIC_KEY } from '../common/decorators/public.decorator';
 
 @Injectable()
 export class TenantGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const req = context.switchToHttp().getRequest<TenantRequest>();
     const tenant = req['tenant'];
 
