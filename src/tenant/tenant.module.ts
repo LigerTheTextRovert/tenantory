b4f91372daecaa758b13ce4b597e8cbbadd6cc05
LiGER <samarghandi112w34@gmail.com>
2026-07-20 18:56:46 +0330

    chore: attach the tenant.module by adding tenant.controller to it

--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tenant } from './entities/tenant.entity';
 import { TenantService } from './tenant.service';
+import { TenantController } from './tenant.controller';
 import { TenantGuard } from './tenant.guard';
 import { TenantInterceptor } from './tenant.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tenant])],
+  controllers: [TenantController],
   providers: [TenantService, TenantGuard, TenantInterceptor],
   exports: [TypeOrmModule, TenantService],
 })
