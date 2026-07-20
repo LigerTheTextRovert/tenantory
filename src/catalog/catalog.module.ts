58bae3f2207a67ac76dff35ebcb4139607ccdb0a
LiGER <samarghandi112w34@gmail.com>
2026-07-20 12:22:35 +0330

    chore: added product.service to catalog.module

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductVariant } from './entities/product-variant.entity';
 import { CategoryModule } from '../category/category.module';
+import { ProductService } from './product/product.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, ProductVariant]),
     CategoryModule,
   ],
+  providers: [ProductService],
   exports: [TypeOrmModule],
 })
 export class CatalogModule {}
