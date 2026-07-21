ed47931744a575b9f3e79d1a11d4d7e1cdc8808d
LiGER <samarghandi112w34@gmail.com>
2026-07-21 23:48:09 +0330

    chore: attach variant.controller and variant.service to catalog.module

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -5,14 +5,16 @@ import { ProductVariant } from './entities/product-variant.entity';
 import { CategoryModule } from '../category/category.module';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
+import { VariantService } from './variant/variant.service';
+import { VariantController } from './variant/variant.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, ProductVariant]),
     CategoryModule,
   ],
-  providers: [ProductService],
-  controllers: [ProductController],
-  exports: [TypeOrmModule],
+  providers: [ProductService, VariantService],
+  controllers: [ProductController, VariantController],
+  exports: [TypeOrmModule, ProductService, VariantService],
 })
 export class CatalogModule {}
