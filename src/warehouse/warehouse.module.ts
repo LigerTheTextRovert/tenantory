c6f26bbabc1a938757d37fa5e81da27aa92ff940
LiGER <samarghandi112w34@gmail.com>
2026-07-23 21:54:10 +0330

    chore: create the warehouse controller

--- src/warehouse/warehouse.module.ts
+++ src/warehouse/warehouse.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Warehouse } from './entities/warehouse.entity';
 import { WarehouseService } from './warehouse.service';
+import { WarehouseController } from './warehouse.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Warehouse])],
   providers: [WarehouseService],
+  controllers: [WarehouseController],
   exports: [TypeOrmModule],
 })
 export class WarehouseModule {}
