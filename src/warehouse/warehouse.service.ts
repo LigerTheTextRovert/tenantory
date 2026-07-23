8e4a5218bc972f8d2ba2914388a7d0116692149a
LiGER <samarghandi112w34@gmail.com>
2026-07-23 22:20:05 +0330

    feat: implement the update method for warehouse.service

--- src/warehouse/warehouse.service.ts
+++ src/warehouse/warehouse.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Warehouse } from './entities/warehouse.entity';
+import { IsNull, Repository } from 'typeorm';
+import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
+import { isUniqueViolation } from '../common/utils/assert-unique.util';
 
 @Injectable()
-export class WarehouseService {}
+export class WarehouseService {
+  constructor(
+    @InjectRepository(Warehouse)
+    private readonly warehouseRepo: Repository<Warehouse>,
+  ) {}
+
+  async findOne(tenantId: string, id: string): Promise<Warehouse> {
+    const warehouse = await this.warehouseRepo.findOne({
+      where: {
+        tenantId,
+        id,
+        deletedAt: IsNull(),
+      },
+    });
+
+    if (!warehouse) {
+      throw new NotFoundException(`There is no warehouse by this ID:${id}`);
+    }
+
+    return warehouse;
+  }
+
+  async update(
+    tenantId: string,
+    id: string,
+    dto: UpdateWarehouseDto,
+  ): Promise<Warehouse> {
+    const warehouse = await this.findOne(tenantId, id);
+
+    if (dto.name) {
+      warehouse.name = dto.name;
+    }
+
+    if (dto.location) {
+      warehouse.location = dto.location;
+    }
+
+    try {
+      await this.warehouseRepo.save(warehouse);
+      return warehouse;
+    } catch (error) {
+      if (isUniqueViolation(error)) {
+        throw new ConflictException('There is a warehouse with this info');
+      }
+      throw error;
+    }
+  }
+}
