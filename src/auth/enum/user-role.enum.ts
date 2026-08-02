1782d25c0195e620863c7005660dbc487613587b
LiGER <samarghandi112w34@gmail.com>
2026-08-02 23:57:53 +0330

    feat: implement user roles enum

---
+++ src/auth/enum/user-role.enum.ts
@@ -0,0 +1,7 @@
+export enum UserRole {
+  SUPER_ADMIN = 'super_admin',
+  TENANT_ADMIN = 'tenant_admin',
+  CATALOG_MANAGER = 'catalog_manager',
+  WAREHOUSE_MANAGER = 'warehouse_manager',
+  CUSTOMER = 'customer',
+}
