5c985ba063277805387dc4f1646e37b7199d0682
LiGER <samarghandi112w34@gmail.com>
2026-08-03 21:15:55 +0330

    feat: implement the core functionality for auth.service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,185 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/register.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcryptjs';
+import { UserRole } from './enum/user-role.enum';
+import { UserResponseDto } from './dto/user-response.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { LoginDto } from './dto/login.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(tenantId: string, dto: RegisterDto) {
+    const user = await this.findOneByEmail(tenantId, dto.email);
+
+    if (user) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    const newUser: User = this.userRepo.create({
+      tenantId,
+      email: dto.email,
+      passwordHash,
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      isActive: true,
+      role: dto.role || UserRole.CUSTOMER,
+    });
+
+    const savedUser = await this.userRepo.save(newUser);
+    return this.toUserResponseDto(savedUser);
+  }
+
+  async login(tenantId: string, dto: LoginDto): Promise<string> {
+    const user = await this.findOneByEmail(tenantId, dto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('Account is deactivated');
+    }
+
+    const isPassportCorrect = await bcrypt.compare(
+      dto.password,
+      user.passwordHash,
+    );
+
+    if (!isPassportCorrect) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const { accessToken } = await this.generateTokens(user);
+
+    return accessToken;
+  }
+
+  async validateJwtPayload(jwtPayload: JwtPayload) {
+    const user = await this.userRepo.findOne({
+      where: { id: jwtPayload.sub, tenantId: jwtPayload.tenantId },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('Account is deactivated');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+      tenantId: user.tenantId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+  }
+
+  async findOneByEmail(tenantId: string, email: string): Promise<User | null> {
+    const user = await this.userRepo.findOne({
+      where: {
+        tenantId,
+        email,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+
+  private toUserResponseDto(user: User): UserResponseDto {
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+      tenantId: user.tenantId,
+      isActive: user.isActive,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+
+  private async generateTokens(user: User) {
+    const payload: JwtPayload = {
+      sub: user.id,
+      tenantId: user.tenantId,
+      email: user.email,
+      role: user.role,
+    };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_ACCESS_SECRET,
+        expiresIn: '15m',
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_REFRESH_SECRET,
+        expiresIn: '15m',
+      }),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  // implement refreshToken, first validate user, then if everything alright, generate a token.
+  async refreshToken(tenantId: string, { refreshToken }: RefreshTokenDto) {
+    const payload = await this.jwtService.verifyAsync<JwtPayload>(
+      refreshToken,
+      {
+        secret: process.env.JWT_REFRESH_SECRET,
+      },
+    );
+
+    const user = await this.userRepo.findOne({
+      where: {
+        tenantId,
+        id: payload.sub,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('Account is deactivated');
+    }
+
+    const tokens = await this.generateTokens(user);
+
+    return {
+      user,
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+    };
+  }
+}
