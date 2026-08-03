3cfc2e877d7c4ec378d3f1ac65a24009ed272043
LiGER <samarghandi112w34@gmail.com>
2026-08-03 12:41:19 +0330

    feat: implement jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,35 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { UnauthorizedException } from '@nestjs/common';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.authService.validateJwtPayload(payload);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    // Attach user to request
+    return {
+      id: payload.sub,
+      email: payload.email,
+      role: payload.role,
+      tenant_id: payload.tenantId,
+    };
+  }
+}
