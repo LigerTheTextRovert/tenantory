1dcfcb8f3eda628cc6c2e0c047c367dba5c3e386
LiGER <samarghandi112w34@gmail.com>
2026-08-03 12:29:15 +0330

    chore: added first and last name to user.entity

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -30,6 +30,12 @@ export class User {
   @Column({ type: 'varchar', length: 255 })
   email: string;
 
+  @Column({ type: 'varchar', length: 255, name: 'first_name' })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'last_name' })
+  lastName: string;
+
   @Column({ type: 'varchar', length: 255, name: 'password_hash' })
   passwordHash: string;
 
