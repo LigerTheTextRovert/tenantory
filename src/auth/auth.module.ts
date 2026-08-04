6d189f164e441a1ab973bb3814766e23cbf80d1f
LiGER <samarghandi112w34@gmail.com>
2026-08-04 13:35:32 +0330

    chore: added the missing imports/providers/controller to the auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,28 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RoleGuard } from './guards/role.guard';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_ACCESS_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard, RoleGuard],
+  controllers: [AuthController],
+})
 export class AuthModule {}
