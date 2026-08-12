1fb5d74056e80348cc2735a7006362127c612ae4
LiGER <samarghandi112w34@gmail.com>
2026-08-12 18:03:46 +0330

    chore: attach the media service and controller to it's module

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MediaController } from './media.controller';
+import { MediaService } from './media.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MediaAsset } from './entities/media-asset.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([MediaAsset])],
+  controllers: [MediaController],
+  providers: [MediaService],
+})
 export class MediaModule {}
