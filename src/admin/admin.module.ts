267457635553d6f7ec24b1aed69eb7cabfdce040
LiGER <samarghandi112w34@gmail.com>
2026-08-22 13:32:28 +0330

    chore: add the services and controllers to admin engine

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TenantSetting } from './entities/tenant-setting.entity';
+import { User } from '../auth/entities/user.entity';
+import { TenantSettingService } from './services/tenant-settings.service';
+import { TenantProvisioningService } from './services/tenant-provisioning.service';
+import { SystemAdminController } from './controllers/system-admin.controller';
+import { TenantAdminController } from './controllers/tenant-admin.controller';
+import { TenantUserManagementService } from './services/tenant-user-management.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([TenantSetting, User])],
+  controllers: [SystemAdminController, TenantAdminController],
+  providers: [
+    TenantSettingService,
+    TenantProvisioningService,
+    TenantUserManagementService,
+  ],
+})
 export class AdminModule {}
