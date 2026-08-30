dc7df87697b7440685740cd412fe832d58fba691
LiGER <samarghandi112w34@gmail.com>
2026-08-30 17:33:57 +0330

    chore: add audit context interceptor in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { TenantGuard } from './tenant/tenant.guard';
 import { TenantInterceptor } from './tenant/tenant.interceptor';
+import { AuditContextInterceptor } from './audit/audit-context.interceptor';
 import { setupSwagger } from './config/swagger.config';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { Logger } from 'nestjs-pino';
@@ -26,6 +27,7 @@ async function bootstrap() {
 
   app.useGlobalGuards(app.get(TenantGuard));
   app.useGlobalInterceptors(app.get(TenantInterceptor));
+  app.useGlobalInterceptors(app.get(AuditContextInterceptor));
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   setupSwagger(app);
