b77d00168d34ac72e5ba6fa96d2bbf47a4b0d8d1
LiGER <samarghandi112w34@gmail.com>
2026-08-30 17:57:10 +0330

    chore: add AUDITOR to UserRole enum

--- src/auth/enum/user-role.enum.ts
+++ src/auth/enum/user-role.enum.ts
@@ -1,6 +1,7 @@
 export enum UserRole {
   SUPER_ADMIN = 'super_admin',
   TENANT_ADMIN = 'tenant_admin',
+  AUDITOR = 'auditor',
   CATALOG_MANAGER = 'catalog_manager',
   WAREHOUSE_MANAGER = 'warehouse_manager',
   CUSTOMER = 'customer',
