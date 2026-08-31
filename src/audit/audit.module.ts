ac7457232d3193f6eb890d2474b6d598392494ae
LiGER <samarghandi112w34@gmail.com>
2026-09-01 01:03:22 +0330

    chore: dont know why i dont have some of the changes

--- src/audit/audit.module.ts
+++ src/audit/audit.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Audit } from './entities/audit.entity';
+import { AuditService } from './audit.service';
+import { AuditListener } from './audit.listener';
+import { AuditController } from './audit.controller';
+import {
+  AuditEventEmitterAdapter,
+  AUDIT_EVENT_EMITTER,
+} from './audit-event-emitter.port';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Audit])],
+  controllers: [AuditController],
+  providers: [
+    AuditService,
+    AuditListener,
+    { provide: AUDIT_EVENT_EMITTER, useClass: AuditEventEmitterAdapter },
+  ],
+  exports: [AuditService],
+})
 export class AuditModule {}
